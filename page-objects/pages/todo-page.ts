98d4e5cf711d0e6273bf7c4b71bb541c5270e3cb
jaana <jaana.jegorova@gmail.com>
2025-03-20 09:28:26 +0200

    feat/jj/TL19

--- page-objects/pages/todo-page.ts
+++ page-objects/pages/todo-page.ts
@@ -8,6 +8,12 @@ export class TodoPage {
   readonly footer: Locator;
   readonly todoList: Locator;
   readonly tasks: Task;
+  readonly activeButton: Locator;
+  readonly completedButton: Locator;
+  readonly clearCompletedButton: Locator;
+  readonly allTasksButton: Locator;
+  readonly todoTaskField: Locator;
+  readonly toggleAll: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -15,6 +21,12 @@ export class TodoPage {
     this.footer = page.getByTestId("footer");
     this.todoList = page.getByTestId("todo-list");
     this.tasks = new Task(this.todoList);
+    this.activeButton = page.getByTestId('[href="#/active"]'); //<a class="selected" href="#/active">Active</a>
+    this.completedButton = page.getByTestId('[href="#/completed"]'); //<a class="" href="#/completed">Completed</"a>
+    this.clearCompletedButton = page.getByTestId(".clear-completed"); //<button class="clear-completed">Clear completed</button>
+    this.allTasksButton = page.locator('[href="#/"]'); //class="selected"
+    this.todoTaskField = page.locator('data-testid="todo-item-label"');
+    this.toggleAll = page.locator('data-testid="toggle-all"');
   }
 
   async open(): Promise<void> {
@@ -30,4 +42,34 @@ export class TodoPage {
     const count = await this.tasks.taskLocator.count();
     expect(count).toBe(expected_count);
   }
+
+  async checkFooterButtonsAreVisible(): Promise<void> {
+    await this.allTasksButton.isVisible();
+    await this.activeButton.isVisible();
+    await this.completedButton.isVisible();
+    await this.clearCompletedButton.isVisible();
+  }
+
+  async checkActiveButtonFunctionality(innerText: string): Promise<void> {
+    const activeButton = this.activeButton.locator("button", {
+      hasText: "Active",
+    });
+    await expect(activeButton).toBeEnabled();
+    await activeButton.hover();
+    await activeButton.click();
+    //const activeTasks = await this.activeButton.locator('.task.active');
+    //await expect(activeTasks).toHaveCountGreaterThan(0);
+    await expect(activeButton).toHaveClass(".selected");
+    expect(this.todoTaskField).not.toBeNull;
+  }
+
+  // async clearCompletedButtonFunctionality(innerText: string): Promise<void> {
+  //   await this.clearCompletedButton.isVisible();
+  //   const locator = this.clearCompletedButton.locator("li", { hasText: innerText });
+  //   await expect(locator).toHaveText(innerText);
+  // }
+
+  // async checkClearCompletedButtonFunctionality(): Promise<void> {
+  //   await ...
+  // }
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-#URL used for testing:
+\_#URL used for testing:
 url = https://todo-app.tallinn-learning.ee
 
 #How to check tests:
 npx playwright test --debug --workers=1
+npx playwright test --project=chromium --debug --workers=1
